1da6c98c851a0c63dd5448d94bcde3b7aafcf698
stupar92 <stupar.etf@gmail.com>
2015-12-02 16:22:15 +0000

    Completed.

--- passportlocal/passportlocal/app.js
+++ passportlocal/passportlocal/app.js
@@ -1,9 +1,13 @@
-﻿var express = require('express');
+﻿// dependencies
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,11 +24,29 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', routes);
-app.use('/users', users);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost:27017/beerlocker1');
+mongoose.connection.on('error', function (err) {
+    console.log('error on mongoose connection');
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ passportlocal/passportlocal/test/test.user.js
@@ -0,0 +1,53 @@
+﻿var should = require("should");
+var mongoose = require('mongoose');
+var assert = require('assert');
+var Account = require("../models/account.js");
+var db;
+
+describe('Account', function () {
+    
+    before(function (done) {
+        db = mongoose.connect('mongodb://localhost/test');
+        done();
+    });
+    
+    after(function (done) {
+        mongoose.connection.close();
+        done();
+    });
+    
+    beforeEach(function (done) {
+        var account = new Account({
+            username: '12345',
+            password: 'testy'
+        });
+        
+        account.save(function (error) {
+            if (error) console.log('error' + error.message);
+            else console.log('no error');
+            done();
+        });
+    });
+    
+    it('find a user by username', function (done) {
+        Account.findOne({ username: '12345' }, function (err, account) {
+            account.username.should.eql('12345');
+            console.log("   username: ", account.username);
+            done();
+        });
+    });
+    
+    it('should not find a user with wrong username', function (done) {
+        Account.findOne({ username: 'wrong' }, function (err, account) {
+            assert.equal(null, account);
+            done();
+        });
+    });
+    
+    afterEach(function (done) {
+        Account.remove({}, function () {
+            done();
+        });
+    });
+
+});
